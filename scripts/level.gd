abe9946537614b917a526a298c6dae3d168e8950
Felix <105539355+FelixSLP@users.noreply.github.com>
2026-04-02 14:21:03 -0500

    changed somebluushti
    AEDROF

--- scripts/level.gd
+++ scripts/level.gd
@@ -9,7 +9,7 @@ func _on_pressed() ->void:
 	print(xp) 
 	if xp >= 20: 
 		xp = 0  
-		leveled_up.emit("You Leveled Up!")
+		leveled_up.emit("You Leveled Up!!!!")
 
 func _on_leveled_up(msg : String) -> void:
 	print(msg)
